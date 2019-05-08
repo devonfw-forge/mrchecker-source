20156f00c737a3581c6a43141db3bc5d211f34db
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2019-05-08 07:56:45 +0200

    final tests up and running

--- mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/core/base/driver/DriverManagerTest_standaloneWiremock.java
+++ mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/core/base/driver/DriverManagerTest_standaloneWiremock.java
@@ -14,11 +14,15 @@ import com.capgemini.mrchecker.test.core.BaseTest;
 import com.capgemini.mrchecker.webapi.core.base.runtime.RuntimeParameters;
 import com.github.tomakehurst.wiremock.client.WireMock;
 
+import java.lang.ProcessBuilder.Redirect;
+
 import java.io.*;
+import java.nio.file.Paths;
 
 public class DriverManagerTest_standaloneWiremock extends BaseTest {
 
-	static Process p = null;
+	private static Process process_wiremock = null;
+	private static final String WIREMOCK_STANDALONE_PORT = "8100";
 
 	@Rule
 	public ExpectedException thrown = ExpectedException.none();
@@ -29,12 +33,12 @@ public class DriverManagerTest_standaloneWiremock extends BaseTest {
 		RuntimeParameters.MOCK_HTTP_HOST.refreshParameterValue();
 		assertEquals("System parameters for 'mock_http_host'", "192.168.1.104", RuntimeParameters.MOCK_HTTP_HOST.getValue());
 		
-		System.setProperty("mock_http_port", "8100");
+		System.setProperty("mock_http_port", WIREMOCK_STANDALONE_PORT);
 		RuntimeParameters.MOCK_HTTP_PORT.refreshParameterValue();
 		assertEquals("Runtime Parameter for MOCK_HTTP_PORT is incorrect", "8100", RuntimeParameters.MOCK_HTTP_PORT.getValue());
 		
 		WireMock driver = DriverManager.getDriverVirtualService();
-		assertEquals("Mock server for http does not run o specified port", 8100, DriverManager.getHttpPort());
+		assertEquals("Mock server for http does not run o specified port", WIREMOCK_STANDALONE_PORT, String.valueOf(DriverManager.getHttpPort()));
 		assertEquals("Mock server for http does not run o specific hostname", "192.168.1.104", DriverManager.getHttpHost());
 	}
 	
@@ -65,42 +69,19 @@ public class DriverManagerTest_standaloneWiremock extends BaseTest {
 	}
 
 	@BeforeClass
-	public static void beforeAll(){
-		File file = new File("c:\\Users\\lucst\\Downloads\\wiremock-standalone-2.21.0.jar");
+	public static void beforeAll() throws IOException {
+		startWiremockStandalone();
 
-		ProcessBuilder pb = new ProcessBuilder("java", "-jar", file.getAbsolutePath(), "--port",  "8100");
-		try {
-			pb.redirectOutput(new File("c:\\Users\\lucst\\Downloads\\output.log"));
-			p = pb.start();
-
-			OutputStream out = p.getOutputStream();
-			ByteArrayOutputStream byte1=new ByteArrayOutputStream();
-			out.write(byte1.toByteArray());
-			String s=byte1.toString();
-			System.out.println("jj: " +s);
-
-			InputStream in = p.getInputStream();
-			InputStream err = p.getErrorStream();
-
-			byte b[]=new byte[in.available()];
-			in.read(b,0,b.length);
-			System.out.println("Input:" + new String(b));
-
-			byte c[]=new byte[err.available()];
-			err.read(c,0,c.length);
-			System.out.println("Error:"  + new String(c));
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
 	}
+
 
 
 	@AfterClass
 	public static void afterAll(){
 		try {
-			p.destroyForcibly();
+			process_wiremock.destroyForcibly();
 		}catch (Exception e){
-
+			//No need to catch Exception
 		}
 
 	}
@@ -118,5 +99,16 @@ public class DriverManagerTest_standaloneWiremock extends BaseTest {
 	public void tearDown() {
 		DriverManager.closeDriverVirtualServer();
 	}
-	
+
+
+	private static void startWiremockStandalone() throws IOException {
+		String path = System.getProperty("user.dir") + Paths.get("/src/test/resources/wiremock/wiremock-standalone-2.21.0.jar");
+		File file = new File(path);
+
+		ProcessBuilder processBuilder = new ProcessBuilder("java", "-jar", file.getAbsolutePath(), "--port", WIREMOCK_STANDALONE_PORT);
+		processBuilder.redirectOutput(Redirect.INHERIT);
+		processBuilder.redirectError(Redirect.INHERIT);
+
+		process_wiremock = processBuilder.start();
+	}
 }
