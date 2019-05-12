bca1cf4ecd9736e9c583a8e1dc8672bbc95f9021
pwojtkow <przemyslaw.wojtkow@capgemini.com>
2019-05-12 16:43:01 +0200

    Added base database module structure. Code moved from old repository.

---
+++ mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/base/database/MySqlDatabase.java
@@ -0,0 +1,10 @@
+package com.capgemini.mrchecker.database.base.database;
+
+import com.capgemini.mrchecker.database.core.BasePageDatabase;
+
+public class MySqlDatabase extends BasePageDatabase {
+
+	@Override public String getDatabaseUnitName() {
+		return "mySqlExample";
+	}
+}
