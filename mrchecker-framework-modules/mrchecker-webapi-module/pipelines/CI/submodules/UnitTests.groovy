ae5f6353af2e59a50927f234b5d8534e1895d470
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2019-05-14 09:42:10 +0200

    downgrade surefire plugin

--- mrchecker-framework-modules/mrchecker-webapi-module/pipelines/CI/submodules/UnitTests.groovy
+++ mrchecker-framework-modules/mrchecker-webapi-module/pipelines/CI/submodules/UnitTests.groovy
@@ -7,12 +7,12 @@ def call(){
             mvn -q surefire:test -Dtest=${env.TEST_NAME} -Dthread.count=${env.THREAD_COUNT} -Denv=${env.ENVIRONMENT} ${env.MVN_PARAMETERS}
             
         """
+        junit "**/${env.APP_WORKSPACE}target/surefire-reports/TEST-*.xml"
         try{
             sh"""
                 cd ${env.APP_WORKSPACE}
                 mvn -q site:site ${env.MVN_PARAMETERS}
             """
-            junit "**/${env.APP_WORKSPACE}target/surefire-reports/TEST-*.xml"
             if (fileExists("${env.APP_WORKSPACE}target/site/allure-report/index.html")) {
                 echo("Before publish allure");
                 publishHTML (target: [allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: "${env.APP_WORKSPACE}target/site/allure-report", reportFiles: 'index.html', reportName: "allure"]);
@@ -21,7 +21,7 @@ def call(){
                 echo("Any HTML report found!");
             }
         } catch (Exception e){
-            echo("No report generated. Reason: \n" + e);
+            error("No report generated. Reason: \n" + e);
         }
     }
 }
