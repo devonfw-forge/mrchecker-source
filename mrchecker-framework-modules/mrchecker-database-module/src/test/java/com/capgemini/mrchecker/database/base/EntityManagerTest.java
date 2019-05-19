61d96a9c3524eae69fc36d8752d9105caecbd4d8
pwojtkow <przemyslaw.wojtkow@capgemini.com>
2019-05-19 18:55:36 +0200

    Cleaning and usng formatter.

--- mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/base/EntityManagerTest.java
+++ mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/base/EntityManagerTest.java
@@ -1,19 +1,13 @@
 package com.capgemini.mrchecker.database.base;
 
 import com.capgemini.mrchecker.database.base.database.MySqlDatabase;
-import com.capgemini.mrchecker.test.core.base.properties.PropertiesSettingsModule;
-import com.google.inject.Guice;
-import com.google.inject.Injector;
 import org.junit.Before;
 import org.junit.Test;
-
-import java.nio.file.Paths;
 
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 public class EntityManagerTest {
-
 
 	private MySqlDatabase mySqlDatabase;
 
