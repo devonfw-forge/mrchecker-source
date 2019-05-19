355a3fb18a229eb0e782ac4b6754a628acd480e6
pwojtkow <przemyslaw.wojtkow@capgemini.com>
2019-05-19 19:07:31 +0200

    Used formatter for all src directory.

--- mrchecker-framework-modules/mrchecker-database-module/src/main/java/com/capgemini/mrchecker/database/core/base/IDao.java
+++ mrchecker-framework-modules/mrchecker-database-module/src/main/java/com/capgemini/mrchecker/database/core/base/IDao.java
@@ -6,11 +6,8 @@ import java.util.List;
 /**
  * Interface which defines main procedures on database.
  *
- * @param <T>
- *            entity based on database table
- * @param <K>
- *            type of id variable in entity
- *
+ * @param <T> entity based on database table
+ * @param <K> type of id variable in entity
  * @author PWOJTKOW
  */
 public interface IDao<T, K extends Serializable> {
@@ -19,8 +16,7 @@ public interface IDao<T, K extends Serializable> {
 	 * Insert new entity into table. If entity is a preexisting managed entity,
 	 * it is ignored by the persist operation.
 	 *
-	 * @param entity
-	 *            - entity to add
+	 * @param entity - entity to add
 	 * @return - entity object added into table
 	 */
 	T save(T entity);
@@ -30,8 +26,7 @@ public interface IDao<T, K extends Serializable> {
 	 * operations on specific database object. Better to use when want to make
 	 * CRUD operations on entity.
 	 *
-	 * @param id
-	 *            - entity id to get from database
+	 * @param id - entity id to get from database
 	 * @return proxy object from database
 	 */
 	T getOne(K id);
@@ -41,8 +36,7 @@ public interface IDao<T, K extends Serializable> {
 	 * After usage this method new entity object is created, based on data from
 	 * this specific row in database.
 	 *
-	 * @param id
-	 *            - entity id to find from database
+	 * @param id - entity id to find from database
 	 * @return entity with data from
 	 */
 	T findOne(K id);
@@ -57,8 +51,7 @@ public interface IDao<T, K extends Serializable> {
 	 * managed entity instance X'-entity is created and the state of X is copied
 	 * into the new managed entity instance X'.
 	 *
-	 * @param entity
-	 *            - entity to update in database.
+	 * @param entity - entity to update in database.
 	 * @return
 	 */
 	T update(T entity);
@@ -66,16 +59,14 @@ public interface IDao<T, K extends Serializable> {
 	/**
 	 * Delete entity from database find by entity object.
 	 *
-	 * @param entity
-	 *            - entity to delete from database.
+	 * @param entity - entity to delete from database.
 	 */
 	void delete(T entity);
 
 	/**
 	 * Delete entity from database find by id.
 	 *
-	 * @param id
-	 *            - specific id number for entity to be deleted.
+	 * @param id - specific id number for entity to be deleted.
 	 */
 	void delete(K id);
 
@@ -93,8 +84,7 @@ public interface IDao<T, K extends Serializable> {
 	/**
 	 * Check is any record in table with specific id.
 	 *
-	 * @param id
-	 *            - specific record id in table.
+	 * @param id - specific record id in table.
 	 * @return - true if record exists, or false id do not.
 	 */
 	boolean exists(K id);
