0eac22aef7b61bbd13c8a59619cb45d518c2a4d6
Lukasz Stefaniszyn <lukasz.stefaniszyn@capgemini.com>
2019-05-20 09:10:00 +0200

    Feature/fix site issue (#8)
    * update site report for maven
    
    * Added validator for Maven and java version
    
    * update UnitTest Stage to address test fail

--- mrchecker-framework-modules/mrchecker-security-module/pipelines/CI/submodules/UnitTests.groovy
+++ mrchecker-framework-modules/mrchecker-security-module/pipelines/CI/submodules/UnitTests.groovy
@@ -1,18 +1,23 @@
 def call(){
 		
 //# Unit test run
-	stage('Unit test'){
-        sh"""
-            cd ${env.APP_WORKSPACE}
-            mvn -q surefire:test -Dtest=${env.TEST_NAME} -Dthread.count=${env.THREAD_COUNT} -Denv=${env.ENVIRONMENT} ${env.MVN_PARAMETERS}
-            
-        """
-        try{
-            sh"""
+    stage('Unit test'){
+        def failStage = false
+        try {
+            sh """
                 cd ${env.APP_WORKSPACE}
-                mvn -q site:site ${env.MVN_PARAMETERS}
+                mvn -q surefire:test -Dtest=${env.TEST_NAME} -Dthread.count=${env.THREAD_COUNT} -Denv=${env.ENVIRONMENT} ${env.MVN_PARAMETERS}
             """
+        } catch (Exception e){
+            failStage = true
+        }
+
+        try{
             junit "**/${env.APP_WORKSPACE}target/surefire-reports/TEST-*.xml"
+            sh"""
+                    cd ${env.APP_WORKSPACE}
+                    mvn -q site:site ${env.MVN_PARAMETERS}
+                """
             if (fileExists("${env.APP_WORKSPACE}target/site/allure-report/index.html")) {
                 echo("Before publish allure");
                 publishHTML (target: [allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: "${env.APP_WORKSPACE}target/site/allure-report", reportFiles: 'index.html', reportName: "allure"]);
@@ -20,8 +25,12 @@ def call(){
             } else {
                 echo("Any HTML report found!");
             }
-        } catch (Exception e){
-            echo("No report generated. Reason: \n" + e);
+        } catch (Exception error){
+            echo("No report generated. Reason: \n" + error);
+        }
+
+        if(failStage){
+            error("Some test cases are failing")
         }
     }
 }
