dee31d1d8236d71124c892e45d33325e058105d5
Mariusz Kowalski <mariusz.kowalski@capgemini.com>
2019-06-04 10:26:12 +0200

    Parametrized

--- mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/Base64Test.java
+++ mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/Base64Test.java
@@ -4,31 +4,27 @@ import com.capgemini.mrchecker.test.core.logger.BFLogger;
 import com.capgemini.mrchecker.webapi.BasePageWebApiTest;
 import com.capgemini.mrchecker.webapi.pages.httbin.Base64Page;
 import io.restassured.response.Response;
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
 import java.text.MessageFormat;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(JUnitParamsRunner.class)
 public class Base64Test extends BasePageWebApiTest {
 	private Base64Page base64Page = new Base64Page();
 
 	@Test
-	public void validateCorrectnessOfBase64Decode_AwesomeText() {
-		String validBase64Data = "SFRUUEJJTiBpcyBhd2Vzb21l";
-		String validBase64Decode = "HTTPBIN is awesome";
-		base64DecodeDataCheck(validBase64Data, validBase64Decode);
-	}
-
-	@Test
-	public void validateCorrectnessOfBase64Decode_MrCheckerText() {
-		String validBase64Data = "TXIuQ2hlY2tlcg==";
-		String validBase64Decode = "Mr.Checker";
-		base64DecodeDataCheck(validBase64Data, validBase64Decode);
-	}
-
-	private void base64DecodeDataCheck(String base64Data, String expectedResult) {
+	@Parameters({
+			"SFRUUEJJTiBpcyBhd2Vzb21l, HTTPBIN is awesome",
+			"TXIuQ2hlY2tlcg==, Mr.Checker",
+			"WmHFvMOzxYLEhyBnxJnFm2zEhSBqYcW6xYQ= , Zażółć gęślą jaźń"
+	})
+	public void sendGetWithBase64DataAndValidateDecodedResponse(String base64Data, String expectedResult) {
 		BFLogger.logInfo(MessageFormat.format("Step 1 - Sending GET query to {0} with valid base 64 data: {1}", base64Page.getEndpoint(), base64Data));
 		Response response = base64Page.sendGETQuery(base64Data);
 
