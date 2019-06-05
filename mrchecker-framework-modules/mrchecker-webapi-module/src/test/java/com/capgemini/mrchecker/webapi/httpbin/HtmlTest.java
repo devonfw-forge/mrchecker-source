e6361f3f97c64f5849e22607797d0321974c3de5
Mariusz Kowalski <mariusz.kowalski@capgemini.com>
2019-06-05 14:12:19 +0200

    Use parser

--- mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/HtmlTest.java
+++ mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/HtmlTest.java
@@ -2,12 +2,10 @@ package com.capgemini.mrchecker.webapi.httpbin;
 
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 import com.capgemini.mrchecker.webapi.BasePageWebApiTest;
+import com.capgemini.mrchecker.webapi.core.utils.HTMLParser;
 import com.capgemini.mrchecker.webapi.pages.httbin.HtmlPage;
 import io.restassured.response.Response;
 import io.restassured.response.ResponseBody;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 import org.junit.Test;
 
 import static org.hamcrest.Matchers.is;
@@ -32,14 +30,12 @@ public class HtmlTest extends BasePageWebApiTest {
 		BFLogger.logInfo("Step 3 - Validate response body is html that contains proper h1 tag content (count and value)");
 		ResponseBody body = response.body();
 		String htmlText = body.asString();
-		Document doc = Jsoup.parse(htmlText);
-		Elements h1 = doc.select("h1");
-		assertThat(h1.size(), is(h1ElementsCount));
-		assertThat(h1.get(0).text(), is(h1ElementText));
+		HTMLParser parser = HTMLParser.parse(htmlText);
+		assertThat(parser.getHeadingElementsCount(1), is(h1ElementsCount));
+		assertThat(parser.getHeadingElementsText(1).get(0), is(h1ElementText));
 
 		BFLogger.logInfo("Step 4 - Validate response body is html that contains proper p tag content (count and length)");
-		Elements p = doc.select("p");
-		assertThat(p.size(), is(pElementCount));
-		assertThat(p.get(0).text().length(), is(pElementTextLength));
+		assertThat(parser.getParagraphElementsCount(), is(pElementCount));
+		assertThat(parser.getParagraphElementsText().get(0).length(), is(pElementTextLength));
 	}
 }
