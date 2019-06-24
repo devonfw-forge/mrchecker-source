85d62899bf0a1fea51f94da61db412d978f2a809
GSTARCZE <gabriel.starczewski@capgemini.com>
2019-06-24 13:01:50 +0200

    Refactoring code after remarks form pull request. Changing formatting and putting parsing logic in a separate class.

--- mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/SimpleXMLTest.java
+++ mrchecker-framework-modules/mrchecker-webapi-module/src/test/java/com/capgemini/mrchecker/webapi/httpbin/SimpleXMLTest.java
@@ -5,10 +5,6 @@ import static org.junit.Assert.assertThat;
 
 import java.io.IOException;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.w3c.dom.Document;
@@ -19,63 +15,64 @@ import org.xml.sax.SAXException;
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 import com.capgemini.mrchecker.webapi.BasePageWebApiTest;
 import com.capgemini.mrchecker.webapi.pages.httbin.SimpleXMLPage;
+import com.capgemini.mrchecker.webapi.pages.utils.SimpleXMLParser;
 
 public class SimpleXMLTest extends BasePageWebApiTest {
 	
-	private static SimpleXMLPage simpleXMLPage ;
-	private static Document simpleXMLPageDocument;
-
+	private static SimpleXMLPage	simpleXMLPage	= new SimpleXMLPage();
+	private static Document			simpleXMLPageDocument;
+	
 	@BeforeClass
-	public static void setup(){
-		simpleXMLPage = new SimpleXMLPage();
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	public static void setup() {
 		BFLogger.logDebug("Reading " + simpleXMLPage.getEndpoint());
 		try {
-			DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-			simpleXMLPageDocument = dBuilder.parse(simpleXMLPage.getXMLDocument().asInputStream());
-			simpleXMLPageDocument.normalizeDocument();
-		} catch (ParserConfigurationException e) {
-			e.printStackTrace();
-		} catch (SAXException e) {
-			e.printStackTrace();
+			simpleXMLPageDocument = SimpleXMLParser.convertToDocumentNormalized(simpleXMLPage.getXMLDocument()
+					.asInputStream());
 		} catch (IOException e) {
-			e.printStackTrace();
+			BFLogger.logInfo("Cannot convert and normalize XML document due to IO Exception. " + e.toString());
+		} catch (SAXException e) {
+			BFLogger.logInfo("Cannot convert XML document due to parsing problem. " + e.toString());
 		}
+		
 	}
+	
 	@Test
 	public void validateXMLPageRequestHTTPResponseCode() {
 		final int HTTP_OK = 200;
 		BFLogger.logInfo("Validating that request for " + simpleXMLPage.getEndpoint() + " completed with  status code: " + HTTP_OK);
-		assertThat(HTTP_OK,equalTo(simpleXMLPage.getXMLDocument().getStatusCode()));
-
+		assertThat(HTTP_OK, equalTo(simpleXMLPage.getXMLDocument()
+				.getStatusCode()));
+		
 	}
+	
 	@Test
 	public void validateRootElementName() {
 		BFLogger.logInfo("Validating that document root is " + simpleXMLPage.getXmlDocumentRoot());
-		assertThat(simpleXMLPage.getXmlDocumentRoot(),equalTo(simpleXMLPageDocument.getDocumentElement().getNodeName()));
+		assertThat(simpleXMLPage.getXmlDocumentRoot(), equalTo(simpleXMLPageDocument.getDocumentElement()
+				.getNodeName()));
 	}
-
+	
 	@Test
 	public void validateElementsNumberWhenSearchingByTag() {
 		BFLogger.logInfo("Validating that item element number is  " + simpleXMLPage.getItemsNumber());
 		NodeList itemsList = simpleXMLPageDocument.getElementsByTagName(simpleXMLPage.getXmlElementItem());
-		assertThat(simpleXMLPage.getItemsNumber(),equalTo(itemsList.getLength()));
+		assertThat(simpleXMLPage.getItemsNumber(), equalTo(itemsList.getLength()));
 	}
-
+	
 	@Test
 	public void validateRootElementAttributeValueAndNumberWhenSearchingByTag() {
 		BFLogger.logInfo("Validating attribute value  " + simpleXMLPage.getXmlElementDate());
 		NodeList itemsList = simpleXMLPageDocument.getElementsByTagName(simpleXMLPage.getXmlDocumentRoot());
-		//there can be only 1
-		assertThat(itemsList.getLength(),equalTo(1));
-		Element rootElement = (Element)(itemsList.item(0));
-
-		assertThat(simpleXMLPage.getXmlElementDateValue(),equalTo(rootElement.getAttribute(simpleXMLPage.getXmlElementDate())));
+		// there can be only 1
+		assertThat(itemsList.getLength(), equalTo(1));
+		Element rootElement = (Element) (itemsList.item(0));
+		
+		assertThat(simpleXMLPage.getXmlElementDateValue(), equalTo(rootElement.getAttribute(simpleXMLPage.getXmlElementDate())));
 	}
-
+	
 	@Test
 	public void validateXmlDocumentEncoding() {
 		BFLogger.logInfo("Validating document encoding  to be " + simpleXMLPageDocument.getXmlEncoding());
-		assertThat(simpleXMLPageDocument.getXmlEncoding(),equalTo(simpleXMLPageDocument.getXmlEncoding()));
+		assertThat(simpleXMLPageDocument.getXmlEncoding(), equalTo(simpleXMLPageDocument.getXmlEncoding()));
 	}
 }
