8cefa0dc5539931ad22f7a1152d6474f22152d88
malgorzata dzienia <malgorzata.dzienia@capgemini.com>
2019-07-04 18:28:41 +0200

    workaround for test cases to run without session exception

--- mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/tests/webElements/ButtonTest.java
+++ mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/tests/webElements/ButtonTest.java
@@ -1,41 +1,39 @@
 package com.capgemini.mrchecker.selenium.core.tests.webElements;
 
-import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertTrue;
-
+import com.capgemini.mrchecker.selenium.core.BasePage;
+import com.capgemini.mrchecker.selenium.core.enums.PageSubURLsEnum;
+import com.capgemini.mrchecker.test.core.BaseTest;
 import org.junit.AfterClass;
 import org.junit.Test;
 import org.openqa.selenium.By;
 
-import com.capgemini.mrchecker.selenium.core.BasePage;
-import com.capgemini.mrchecker.selenium.core.enums.PageSubURLsEnum;
-import com.capgemini.mrchecker.test.core.BaseTest;
+import static junit.framework.TestCase.assertEquals;
+import static junit.framework.TestCase.assertTrue;
 
 /**
  * Created by LKURZAJ on 03.03.2017.
  */
 public class ButtonTest extends BaseTest {
-	
-	private static By buttonSubmit = By.cssSelector("button#submit");
-	
+	private        QuickFixSeleniumPage quickFixSeleniumPage = new QuickFixSeleniumPage();
+	private static By                   buttonSubmit         = By.cssSelector("button#submit");
+
 	@AfterClass
 	public static void tearDownAll() {
-		BasePage.getDriver()
-				.close();
+
 	}
-	
+
 	@Test
 	public void test() {
 		// check if element is displayed
 		assertTrue(BasePage.getDriver()
 				.elementButton(ButtonTest.buttonSubmit)
 				.isDisplayed());
-		
+
 		// check if element type equals Button
 		assertEquals("Button", BasePage.getDriver()
 				.elementButton(ButtonTest.buttonSubmit)
 				.getElementTypeName());
-		
+
 		// click on button and verify if url was changed
 		BasePage.getDriver()
 				.elementButton(ButtonTest.buttonSubmit)
@@ -44,14 +42,15 @@ public class ButtonTest extends BaseTest {
 				.getCurrentUrl()
 				.contains("&submit="));
 	}
-	
+
 	@Override
 	public void setUp() {
+		quickFixSeleniumPage = new QuickFixSeleniumPage();
 		BasePage.getDriver()
 				.get(PageSubURLsEnum.TOOLS_QA.subURL() + PageSubURLsEnum.AUTOMATION_PRACTICE_FORM.subURL());
 		return;
 	}
-	
+
 	@Override
 	public void tearDown() {
 		// TODO Auto-generated method stub
