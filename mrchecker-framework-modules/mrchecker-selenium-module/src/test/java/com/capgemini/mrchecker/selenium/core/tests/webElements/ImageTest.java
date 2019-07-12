394e037f2c00e4f88c4be074a7abf8395a01f40f
Malgorzata Dzienia <39510143+MalgorzataDzienia@users.noreply.github.com>
2019-07-12 11:15:57 +0200

    workaround for test cases to run without session exception (#16)

--- mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/tests/webElements/ImageTest.java
+++ mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/tests/webElements/ImageTest.java
@@ -13,31 +13,31 @@ import static junit.framework.TestCase.assertTrue;
  * Created by TTRZCINSKI on 19.10.2018.
  */
 public class ImageTest extends BaseTest {
-    private static By img1 = By.cssSelector("img");
+	QuickFixSeleniumPage quickFixSeleniumPage = new QuickFixSeleniumPage();
+	private static By img1 = By.cssSelector("img");
 
-    @AfterClass
-    public static void tearDownAll() {
-        BasePage.getDriver()
-                .close();
-    }
+	@AfterClass
+	public static void tearDownAll() {
 
-    @Test
-    public void test() {
-        // check if label is displayed
-        assertTrue(BasePage.getDriver()
-                .elementImage(ImageTest.img1)
-                .isDisplayed());
-    }
+	}
 
-    @Override
-    public void setUp() {
-        BasePage.getDriver()
-                .get(PageSubURLsEnum.TOOLS_QA.subURL() + PageSubURLsEnum.AUTOMATION_PRACTICE_FORM.subURL());
-        return;
-    }
+	@Test
+	public void test() {
+		// check if label is displayed
+		assertTrue(BasePage.getDriver()
+				.elementImage(ImageTest.img1)
+				.isDisplayed());
+	}
 
-    @Override
-    public void tearDown() {
-        // TODO Auto-generated method stub
-    }
+	@Override
+	public void setUp() {
+		BasePage.getDriver()
+				.get(PageSubURLsEnum.TOOLS_QA.subURL() + PageSubURLsEnum.AUTOMATION_PRACTICE_FORM.subURL());
+		return;
+	}
+
+	@Override
+	public void tearDown() {
+		// TODO Auto-generated method stub
+	}
 }
---
+++ mrchecker-framework-modules/mrchecker-selenium-module/src/test/java/com/capgemini/mrchecker/selenium/core/tests/webElements/QuickFixSeleniumPage.java
@@ -0,0 +1,24 @@
+package com.capgemini.mrchecker.selenium.core.tests.webElements;
+
+import com.capgemini.mrchecker.selenium.core.BasePage;
+
+public class QuickFixSeleniumPage extends BasePage {
+	//This page is created to fix Selenium Session Exception from tes.WebElements test cases.
+	//When the test cases are refactored, this page should be deleted.
+
+	@Override
+	public boolean isLoaded() {
+		getDriver().waitForPageLoaded();
+		return true;
+	}
+
+	@Override
+	public void load() {
+
+	}
+
+	@Override
+	public String pageTitle() {
+		return getActualPageTitle();
+	}
+}
