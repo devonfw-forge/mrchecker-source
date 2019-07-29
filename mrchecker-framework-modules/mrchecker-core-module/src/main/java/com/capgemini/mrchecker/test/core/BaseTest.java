fa8d96dcc736fb85414b9919ba399ca93e355816
KMariusz <kowalski.mariusz.87@gmail.com>
2019-07-29 14:04:55 -0700

    Default env param (#15)

--- mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/BaseTest.java
+++ mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/BaseTest.java
@@ -1,15 +1,5 @@
 package com.capgemini.mrchecker.test.core;
 
-import java.net.MalformedURLException;
-
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.rules.TestWatcher;
-import org.junit.runner.RunWith;
-
 import com.capgemini.mrchecker.test.core.BaseTestWatcher.TestClassRule;
 import com.capgemini.mrchecker.test.core.analytics.AnalyticsProvider;
 import com.capgemini.mrchecker.test.core.analytics.IAnalytics;
@@ -23,41 +13,47 @@ import com.capgemini.mrchecker.test.core.base.runtime.RuntimeParametersCore;
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 import com.capgemini.mrchecker.test.core.testRunners.ParallelTestClassRunner;
 import com.google.inject.Guice;
+import org.junit.*;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.RunWith;
+
+import java.net.MalformedURLException;
 
 @RunWith(ParallelTestClassRunner.class)
 public abstract class BaseTest implements IBaseTest {
-	
-	private static IEnvironmentService	environmentService;
-	private final static IAnalytics		analytics;
-	
+
+	private static       IEnvironmentService environmentService;
+	private final static IAnalytics          analytics;
+
 	private final static PropertiesCoreTest setPropertiesSettings;
+
 	static {
 		setPropertiesSettings = setPropertiesSettings();
-		setRuntimeParametersCore();
+		setRuntimeParametersCore(setPropertiesSettings.getDefaultEnvironmentName());
 		setEnvironmetInstance(setPropertiesSettings.isEncryptionEnabled());
 		analytics = setAnalytics(setPropertiesSettings.isAnalyticsEnabled());
 	}
-	
+
 	public BaseTest() {
 		getAnalytics().sendClassName();
 	}
-	
+
 	public static IEnvironmentService getEnvironmentService() {
 		return BaseTest.environmentService;
 	}
-	
+
 	public static IAnalytics getAnalytics() {
 		return BaseTest.analytics;
 	}
-	
+
 	public static void setEnvironmentService(IEnvironmentService environmentService) {
 		BaseTest.environmentService = environmentService;
 	}
-	
+
 	@BeforeClass
 	public static final void setUpClass() throws MalformedURLException {
 	}
-	
+
 	@AfterClass
 	public static final void tearDownClass() {
 		BFLogger.logDebug("BaseTest.tearDownClass()");
@@ -65,41 +61,41 @@ public abstract class BaseTest implements IBaseTest {
 				.toString());
 		BFLogger.logDebug("TestClassRule.classObservers: " + TestClassRule.classObservers.get()
 				.toString());
-		
+
 		// Run observers
 		TestClassRule.classObservers.get()
 				.forEach(ITestObserver::onTestClassFinish);
 		BaseTestWatcher.observers.get()
 				.forEach(ITestObserver::onTestClassFinish);
-		
+
 		// Clear observers for all tests
 		BaseTestWatcher.observers.get()
 				.clear();
 		TestClassRule.classObservers.get()
 				.clear();
 		BFLogger.logDebug("All observers cleared.");
-		
+
 	}
-	
+
 	@After
 	public void tearDownTestLast() {
-		
+
 	}
-	
+
 	@Override
 	abstract public void setUp();
-	
+
 	@Override
 	abstract public void tearDown();
-	
+
 	// Repacks baseTestWatch to allow Cucumber runner
-	private BaseTestWatcher	baseTestWatcher	= new BaseTestWatcher(this);
+	private BaseTestWatcher baseTestWatcher = new BaseTestWatcher(this);
 	@Rule
-	public TestWatcher		testWatcher		= getBaseTestWatcher();
-	
+	public  TestWatcher     testWatcher     = getBaseTestWatcher();
+
 	@ClassRule
 	public static TestClassRule classRule = new TestClassRule();
-	
+
 	private static void setEnvironmetInstance(boolean isEncryptionEnabled) {
 		// Environment variables either from environmnets.csv or any other input data.
 		IEnvironmentService environmentInstance = Guice.createInjector(new EnvironmentModule())
@@ -112,33 +108,35 @@ public abstract class BaseTest implements IBaseTest {
 		}
 		BaseTest.setEnvironmentService(environmentInstance);
 	}
-	
-	private static void setRuntimeParametersCore() {
+
+	private static void setRuntimeParametersCore(String defaultEnvironmentName) {
+		RuntimeParametersCore.ENV.setDefaultValue(defaultEnvironmentName);
+		RuntimeParametersCore.ENV.refreshParameterValue();
 		// Read System or maven parameters
 		BFLogger.logDebug(RuntimeParametersCore.ENV.toString());
 	}
-	
+
 	private static PropertiesCoreTest setPropertiesSettings() {
 		/*
 		 * For now there is no properties settings file for Core module. In future, please have a look on Selenium
 		 * Module PropertiesSelenium propertiesSelenium = Guice.createInjector(PropertiesSettingsModule.init())
 		 * .getInstance(PropertiesSelenium.class);
 		 */
-		
+
 		// Get and then set properties information from settings.properties file
 		PropertiesCoreTest propertiesCoreTest = Guice.createInjector(PropertiesSettingsModule.init())
 				.getInstance(PropertiesCoreTest.class);
 		return propertiesCoreTest;
 	}
-	
+
 	private static IAnalytics setAnalytics(Boolean isAnalyticsEnabled) {
 		BFLogger.logAnalytics("Is analytics enabled:" + isAnalyticsEnabled);
 		return isAnalyticsEnabled ? AnalyticsProvider.DISABLED : AnalyticsProvider.DISABLED;
-		
+
 	}
-	
+
 	public BaseTestWatcher getBaseTestWatcher() {
 		return baseTestWatcher;
 	}
-	
+
 }
