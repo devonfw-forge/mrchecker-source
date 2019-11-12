e3f97b81dd6de46c125cf468bf89c97c01aeea04
Lukasz Stefaniszyn <stefaniszynlukasz@gmail.com>
2019-11-12 07:33:27 +0100

    test validation

--- mrchecker-framework-modules/mrchecker-mobile-module/src/test/java/com/capgemini/mrchecker/mobile/BasePageTest.java
+++ mrchecker-framework-modules/mrchecker-mobile-module/src/test/java/com/capgemini/mrchecker/mobile/BasePageTest.java
@@ -4,11 +4,7 @@ import com.capgemini.mrchecker.mobile.core.BasePage;
 import com.capgemini.mrchecker.test.core.logger.BFLogger;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 
-import java.io.File;
-import java.io.IOException;
 
 public class BasePageTest {
 
@@ -22,17 +18,13 @@ public class BasePageTest {
 
 		public String myMethod() {
 			BFLogger.logDebug("TITLE: " + getDriver().getPageSource());
-			BFLogger.logDebug("AppStringMap: " + getDriver().getAppStringMap());
 
-			File file  = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
-			try {
-				FileUtils.copyFile(file, new File("Screenshot.jpg"));
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-
-			//			getDriver().getTitle();
-//			getDriver().getCurrentUrl();
+//			File file  = ((TakesScreenshot)getDriver()).getScreenshotAs(OutputType.FILE);
+//			try {
+//				FileUtils.copyFile(file, new File("Screenshot.jpg"));
+//			} catch (IOException e) {
+//				e.printStackTrace();
+//			}
 			return "Welcome";
 		}
 	}
