474052b291ef9063408a3b545f348dd29db6c3f9
Marcel Rzepka <marzepka@capgemini.com>
2020-09-23 00:07:26 +0200

    change quotes type

--- CICD/DeployDispatcher.groovy
+++ CICD/DeployDispatcher.groovy
@@ -8,13 +8,10 @@ try{
 } catch (Exception e){
     stage("Fall back to default"){
         ansiColor{
-            print ("""
-\u001b[37m
-\u001b[41;1m
+            print ('''
             This is only default script. That means you're branch has no correct            
             jenkins file for this job. If any customization is needed fix this.             
-\u001b[0m
-            """)
+            ''')
         }
     }
 } finally {
