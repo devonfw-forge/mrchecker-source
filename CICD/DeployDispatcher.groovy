a4ae29e79edf2acc709d7c9304fb496867524197
Marcel Rzepka <marzepka@capgemini.com>
2020-09-22 23:39:59 +0200

    try with double escape

--- CICD/DeployDispatcher.groovy
+++ CICD/DeployDispatcher.groovy
@@ -9,11 +9,11 @@ try{
     stage("Fall back to default"){
         ansiColor{
             echo """
-\u001b[37m
-\u001b[41;1m
+\\u001b[37m
+\\u001b[41;1m
             This is only default script. That means you're branch has no correct            
             jenkins file for this job. If any customization is needed fix this.             
-\u001b[0m
+\\u001b[0m
             """
         }
     }
