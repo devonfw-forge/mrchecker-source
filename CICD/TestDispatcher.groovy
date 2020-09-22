626739288993e24829f118d047ab5147289d47fd
Marcel Rzepka <marzepka@capgemini.com>
2020-09-23 00:04:58 +0200

    echo -> print

--- CICD/TestDispatcher.groovy
+++ CICD/TestDispatcher.groovy
@@ -9,10 +9,13 @@ try{
 } catch (Exception e){
     stage("Fall back to default"){
         ansiColor{
-            echo """
+            print ("""
+\u001b[37m
+\u001b[41;1m
             This is only default script. That means you're branch has no correct            
             jenkins file for this job. If any customization is needed fix this.             
-            """
+\u001b[0m
+            """)
         }
     }
 } finally {
