e1a759789c209f71e7273c486c3abc189de6197c
Marcel Rzepka <marzepka@capgemini.com>
2020-09-29 14:42:13 +0200

    try to use single node

--- CICD/BuildDispatcher.groovy
+++ CICD/BuildDispatcher.groovy
@@ -1,25 +1,24 @@
-try{
-    node('master'){
-        stage('See if there is Jenkinsfile'){
-            checkout scm
-        }
-        load 'CICD/Build_Jenkinsfile'
-    }
-} catch (Exception e){
-    stage("Fall back to default"){
-        ansiColor{
-            print (\'\'\'
-            This is only default script. That means you're branch has no correct            
-            jenkins file for this job. If any customization is needed fix this.             
-            \'\'\')
-        }
-    }
-} finally {
-    node('master'){
-        configFileProvider([configFile(fileId: 'buildDefault', variable: 'buildDefault')]) {
-            load buildDefault
+pipeline{
+    agent {docker {image 'mrchecker/mrchecker:v1.1.3'}}
+    stages{
+        stage("see if there is jenkinsfile"){
+            try{
+                    checkout scm
+                    load 'CICD/Build_Jenkinsfile'
+
+            } catch (Exception e){
+                    ansiColor{
+                        print (\'\'\'
+                        This is only default script. That means you're branch has no correct            
+                        jenkins file for this job. If any customization is needed fix this.             
+                        \'\'\')
+                    }
+            } finally {
+                    configFileProvider([configFile(fileId: 'buildDefault', variable: 'buildDefault')]) {
+                        load buildDefault
+                    }
+            }
         }
     }
 }
-
 //https://github.com/jenkinsci/pipeline-multibranch-defaults-plugin/blob/master/README.md
