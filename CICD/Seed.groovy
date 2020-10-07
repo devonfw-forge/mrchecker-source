d5f0fc50bf975c45a641f6216b3af8c658d56c6b
Marcel Rzepka <marzepka@capgemini.com>
2020-10-07 23:54:23 +0200

    add special case for deploing parent pom

--- CICD/Seed.groovy
+++ CICD/Seed.groovy
@@ -30,12 +30,15 @@ configFiles << new ConfigFile(id:'testDispatcher',name:'testDispatcher',comment:
 configFiles << new ConfigFile(id:'deployDispatcher',name:'deployDispatcher',comment:'script for providing default fallback if no pipelinescript found in repo',location:'CICD/DeployDispatcher.groovy')
 
 configFiles << new ConfigFile(id:'buildDefault',name:'buildDefault',comment:'default script for BUILD stage',location:'CICD/Build_Jenkinsfile')
-configFiles << new ConfigFile(id:'testDefault',name:'testDefault',comment:'default script for BUILD stage',location:'CICD/Test_Jenkinsfile')
-configFiles << new ConfigFile(id:'deployDefault',name:'deployDefault',comment:'default script for BUILD stage',location:'CICD/Deploy_Jenkinsfile')
+configFiles << new ConfigFile(id:'testDefault',name:'testDefault',comment:'default script for TEST stage',location:'CICD/Test_Jenkinsfile')
+configFiles << new ConfigFile(id:'deployDefault',name:'deployDefault',comment:'default script for DEPLOY stage',location:'CICD/Deploy_Jenkinsfile')
+
+configFiles << new ConfigFile(id:'deployParentJenkinsfile',name:'deployParentJenkinsfile',comment:'default script for DEPLOY stage for parent pom',location:'CICD/Deploy_Parent_Jenkinsfile')
 
     def script = makeJobs(configs,folders)
     //script += makeViews(modules,branchesWithView)
     script += makeConfigFiles(configFiles)
+    script += addJobForDeployParent()
 print '\n'*5+'-'*80+'\n\t\t\tSCRIPT\n'
 print script
 print '\n'*5+'-'*80+'\n\t\t\tEND\n'
@@ -43,6 +46,47 @@ node{
     jobDsl scriptText: script
 }
 
+def addJobForDeployParent(){
+    return """
+    multibranchPipelineJob("deploy/mrchecker-test-framework"){
+        description("Build source code and provide packages")
+        branchSources{
+            branchSource{
+                source{
+                    git{
+                        id('12314')
+                        remote('https://github.com/devonfw-forge/mrchecker-source.git')
+                        traits{
+                            gitBranchDiscovery()
+                        }
+                    }
+                }
+                strategy{
+                    defaultBranchPropertyStrategy {
+                        props{
+                            noTriggerBranchProperty()
+                        }
+                    }
+                }
+            }
+        }
+        orphanedItemStrategy{
+            discardOldItems{
+                daysToKeep(30)
+            }
+        }
+        factory{
+            pipelineBranchDefaultsProjectFactory {
+                scriptId('deployParentJenkinsfile')
+                useSandbox(false)
+            }
+        }
+        triggers{
+            cron("")
+        }
+    }
+    """
+}
 def makeConfigFiles(cfgFiles){
     def script = ""
     node('master'){
