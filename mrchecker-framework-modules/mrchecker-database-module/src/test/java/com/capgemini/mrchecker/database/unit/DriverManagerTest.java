37a813edeae487273f5a0f3391a1d35de214f901
Pyskaczu <michal.babiarz84@gmail.com>
2020-11-17 16:36:11 +0100

    DB - adds missing unit tests

--- mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/unit/DriverManagerTest.java
+++ mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/unit/DriverManagerTest.java
@@ -1,6 +1,7 @@
 package com.capgemini.mrchecker.database.unit;
 
 import static com.capgemini.mrchecker.database.mocks.PersistenceProviderMock.SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK;
+import static com.capgemini.mrchecker.database.utils.IsCollectionDistinct.isCollectionDistinct;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 import static org.hamcrest.core.IsEqual.equalTo;
@@ -8,6 +9,8 @@ import static org.hamcrest.core.IsNot.not;
 import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.EntityManager;
@@ -15,17 +18,18 @@ import javax.persistence.EntityManager;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import com.capgemini.mrchecker.database.core.DriverManager;
 import com.capgemini.mrchecker.database.tags.UnitTest;
+import com.capgemini.mrchecker.database.utils.ConcurrencyUtils;
 
 @UnitTest
 public class DriverManagerTest {
-	public static EntityManager	test_em1;
-	public static EntityManager	test_em2;
-	
+	private static EntityManager	test_em1;
+
+	private final List<EntityManager> managers = new ArrayList<>();
+
 	@BeforeEach
 	public void init() {
 		DriverManager.closeDriver();
@@ -33,13 +37,8 @@ public class DriverManagerTest {
 	
 	@AfterEach
 	public void cleanup() {
-		if (!Objects.isNull(test_em1)) {
-			test_em1.close();
-		}
-		
-		if (!Objects.isNull(test_em2)) {
-			test_em2.close();
-		}
+		managers.forEach(EntityManager::close);
+		managers.clear();
 	}
 	
 	@AfterAll
@@ -50,15 +49,18 @@ public class DriverManagerTest {
 	@Test
 	public void shouldCreateEntityManager() {
 		test_em1 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
-		
+		managers.add(test_em1);
+
 		assertThat(test_em1, is(notNullValue()));
 	}
 	
 	@Test
 	public void shouldCreateEntityManagerCalledTwiceReturnDifferentInstances() {
 		test_em1 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
-		test_em2 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
-		
+		EntityManager test_em2 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
+		managers.add(test_em1);
+		managers.add(test_em2);
+
 		assertThat(test_em1, is(notNullValue()));
 		assertThat(test_em1, is(not(equalTo(test_em2))));
 	}
@@ -66,6 +68,7 @@ public class DriverManagerTest {
 	@Test
 	public void shouldCloseDriver() {
 		test_em1 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
+		managers.add(test_em1);
 		assertThat(test_em1, is(notNullValue()));
 		
 		DriverManager.closeDriver();
@@ -76,6 +79,7 @@ public class DriverManagerTest {
 	@Test
 	public void shouldCloseDriverTwice() {
 		test_em1 = DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK);
+		managers.add(test_em1);
 		assertThat(test_em1, is(notNullValue()));
 		
 		DriverManager.closeDriver();
@@ -85,8 +89,8 @@ public class DriverManagerTest {
 	}
 	
 	@Test
-	@Disabled
-	// TODO: implement that
-	public void shouldCreateEntityManagerMultithreaded() {
+	public void shouldCreateEntityManagerMultithreaded() throws InterruptedException {
+		managers.addAll(ConcurrencyUtils.getInstancesConcurrently(() -> DriverManager.createEntityManager(SUPPORTED_PERSISTENCE_UNIT_NAME_FOR_MOCK)));
+		assertThat(managers, isCollectionDistinct());
 	}
 }
---
+++ mrchecker-framework-modules/mrchecker-database-module/src/test/java/com/capgemini/mrchecker/database/utils/IsCollectionDistinct.java
@@ -0,0 +1,40 @@
+package com.capgemini.mrchecker.database.utils;
+
+import org.hamcrest.BaseMatcher;
+import org.hamcrest.Description;
+import org.hamcrest.Factory;
+import org.hamcrest.Matcher;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class IsCollectionDistinct<T> extends BaseMatcher<T> {
+
+    private final List<Object> duplicates = new ArrayList<>();
+
+    @Override
+    public boolean matches(Object o) {
+        if(!(o instanceof Iterable)) {
+          throw new IllegalArgumentException("Must be of Iterable type");
+        }
+
+        Iterator<Object> iterator = ((Iterable<Object>) o).iterator();
+        Set<Object> uniqueValues = new HashSet<>();
+        ((Iterable<Object>) o).forEach(object -> {if(!uniqueValues.add(object))  duplicates.add(object);});
+
+        return duplicates.isEmpty();
+    }
+
+    @Override
+    public void describeTo(Description description) {
+        description.appendText("No duplicates but found: " + duplicates.toString());
+    }
+
+    @Factory
+    public static <T> Matcher<T> isCollectionDistinct() {
+        return new IsCollectionDistinct();
+    }
+}
