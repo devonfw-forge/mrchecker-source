9ef01941676ba9f0364dd15718ee69547c512e10
Marcel Rzepka <marzepka@capgemini.com>
2020-11-24 16:46:11 +0100

    print exception

--- CICD/TestDispatcher.groovy
+++ CICD/TestDispatcher.groovy
@@ -9,6 +9,7 @@ try{
 } catch (Exception e){
     stage("Fall back to default"){
         ansiColor{
+            print('DException ${e}')
             print (\'\'\'
             This is only default script. That means you're branch has no correct            
             jenkins file for this job. If any customization is needed fix this.             
