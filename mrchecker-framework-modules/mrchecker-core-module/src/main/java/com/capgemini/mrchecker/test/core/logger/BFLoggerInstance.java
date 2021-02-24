c2051f2b617e8f7b24d3ff27c47cd3839f260d9b
Marcel Rzepka <marzepka@capgemini.com>
2021-02-24 16:19:52 +0100

    add separate logs

--- mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/logger/BFLoggerInstance.java
+++ mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/logger/BFLoggerInstance.java
@@ -1,33 +1,31 @@
 package com.capgemini.mrchecker.test.core.logger;
 
-import com.capgemini.mrchecker.test.core.base.environment.EnvironmentModule;
+import com.google.common.base.Charsets;
+import com.google.common.io.Files;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.logging.log4j.ThreadContext;
-import org.apache.logging.log4j.core.Appender;
-import org.apache.logging.log4j.core.Filter;
-import org.apache.logging.log4j.core.appender.FileAppender;
 import org.apache.logging.log4j.core.config.AppenderRef;
-import org.apache.logging.log4j.core.config.builder.api.FilterComponentBuilder;
-import org.apache.logging.log4j.spi.LoggerContext;
-import org.apache.logging.log4j.spi.LoggerContextFactory;
-import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 import org.apache.logging.log4j.core.config.LoggerConfig;
 
 import java.io.File;
-import java.net.URI;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;
 
 public class BFLoggerInstance {
+//    static {
+//        //LogManager.getRootLogger().
+//    }
     private Logger logger;
     //private File logFile;
 
     private static final String	FBEGIN	= "Function: ";
     private static final String	FEND	= "END";
-    private static final String LOG_PATTERN = "%d{yyyy-MM-dd 'at' HH:mm:ss z} %M - %m%x%n";
     public BFLoggerInstance(){
-        System.err.println("[MRZ] BFLogger instance "+Thread.currentThread().getName());
+
         ThreadContext.put("threadName",Thread.currentThread().getName());
         getLogger();
     }
@@ -36,12 +34,6 @@ public class BFLoggerInstance {
     }
     private Logger getLogger(){
         //LoggerConfig l = new LoggerConfig.RootLogger();
-        List<AppenderRef> l = new LoggerConfig().getAppenderRefs();
-        System.err.println("L: "+l.size());
-        for (AppenderRef ar: l){
-            System.err.println("MRZ app ref: "+ar);
-        }
-
 //        Appender LogFileAppender = FileAppender.newBuilder().setName(getFileLogName()).build();
 //         uilder().build();
 //        Filter LogFileFilter = new FilterComponentBuilder().build() ;
@@ -49,7 +41,8 @@ public class BFLoggerInstance {
 //        l.addAppender(LogFileAppender,Level.DEBUG, null);
 
         if (logger == null) {
-            logger = LogManager.getLogger("thread");
+            //logger = LogManager.getLogger("thread");
+            logger = LogManager.getRootLogger();
             //logger = LogManager.getLogger();
         }
         return logger;
@@ -75,11 +68,34 @@ public class BFLoggerInstance {
     public void logAnalytics(String message){
         log(Level.forName("ANALYTICS",800),message);
     }
+
+    private String getAppenderName(){
+        return ThreadContext.get("threadName");
+    }
+    protected File getDirectory(){
+        File directory = new File("./logs");
+        return directory;
+    }
+    private File getLogFile() {
+        File logFile = new File(getDirectory().getPath(), getAppenderName() + ".log");
+        //logFile.deleteOnExit();
+
+        return logFile;
+    }
     public void startSeparateLog(){
-        //TODO: make it work
+        try {
+            PrintWriter pw = new PrintWriter(getLogFile());
+            pw.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
     }
     public String dumpSeparateLog(){
-        //TODO: make it work
-        return "test";
+        try {
+            return Files.toString(getLogFile(), Charsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 }
