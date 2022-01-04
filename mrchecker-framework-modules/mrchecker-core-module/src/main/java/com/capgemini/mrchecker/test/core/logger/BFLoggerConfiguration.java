7c09a03914bae66c49de6e57c260cda21715cdca
Michal Babiarz <michal.babiarz84@gmail.com>
2022-01-04 14:57:07 +0100

    Adds ThreadContext.put to BFLoggerConfiguration

--- mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/logger/BFLoggerConfiguration.java
+++ mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/logger/BFLoggerConfiguration.java
@@ -1,5 +1,6 @@
 package com.capgemini.mrchecker.test.core.logger;
 
+import org.apache.logging.log4j.ThreadContext;
 import org.apache.logging.log4j.core.LoggerContext;
 import org.apache.logging.log4j.core.config.ConfigurationSource;
 import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
@@ -7,5 +8,6 @@ import org.apache.logging.log4j.core.config.xml.XmlConfiguration;
 public class BFLoggerConfiguration extends XmlConfiguration {
 	public BFLoggerConfiguration(LoggerContext context, ConfigurationSource configSource) {
 		super(context, configSource);
+		ThreadContext.put("threadName", "main");
 	}
 }
