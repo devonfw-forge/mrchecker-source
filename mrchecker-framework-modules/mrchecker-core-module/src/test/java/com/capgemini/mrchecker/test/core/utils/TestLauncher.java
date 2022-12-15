6d90aba9d3bf21dd155e840060c2316869e74701
Mariusz Kowalski <mariusz.kowalski@capgemini.com>
2022-12-15 10:08:37 +0100

    Bumped all - before Selenium 4

--- mrchecker-framework-modules/mrchecker-core-module/src/test/java/com/capgemini/mrchecker/test/core/utils/TestLauncher.java
+++ mrchecker-framework-modules/mrchecker-core-module/src/test/java/com/capgemini/mrchecker/test/core/utils/TestLauncher.java
@@ -1,9 +1,6 @@
 package com.capgemini.mrchecker.test.core.utils;
 
-import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
-
-import java.util.List;
-
+import com.capgemini.mrchecker.test.core.BaseTest;
 import org.junit.platform.launcher.Launcher;
 import org.junit.platform.launcher.LauncherDiscoveryRequest;
 import org.junit.platform.launcher.TestPlan;
@@ -12,38 +9,38 @@ import org.junit.platform.launcher.core.LauncherFactory;
 import org.junit.platform.launcher.listeners.SummaryGeneratingListener;
 import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
-import com.capgemini.mrchecker.test.core.BaseTest;
+import java.util.Collections;
+import java.util.List;
 
-import edu.emory.mathcs.backport.java.util.Collections;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
 
 public final class TestLauncher {
-	
-	private TestLauncher() {
-	}
-	
-	public static TestExecutionSummary launch(Class<? extends BaseTest> testClass) {
-		
-		return launch(Collections.singletonList(testClass));
-	}
-	
-	public static TestExecutionSummary launch(String className) throws ClassNotFoundException {
-		
-		return launch((Class<? extends BaseTest>) Class.forName(className));
-	}
-	
-	public static TestExecutionSummary launch(List<Class<? extends BaseTest>> testClasses) {
-		SummaryGeneratingListener listener = new SummaryGeneratingListener();
-		
-		LauncherDiscoveryRequestBuilder requestBuilder = LauncherDiscoveryRequestBuilder.request();
-		testClasses
-				.forEach(testClass -> requestBuilder.selectors(selectClass(testClass)));
-		
-		LauncherDiscoveryRequest request = requestBuilder.build();
-		Launcher launcher = LauncherFactory.create();
-		TestPlan testPlan = launcher.discover(request);
-		launcher.registerTestExecutionListeners(listener);
-		launcher.execute(request);
-		
-		return listener.getSummary();
-	}
+
+    private TestLauncher() {
+    }
+
+    public static TestExecutionSummary launch(Class<? extends BaseTest> testClass) {
+        return launch(Collections.singletonList(testClass));
+    }
+
+    public static TestExecutionSummary launch(String className) throws ClassNotFoundException {
+
+        return launch((Class<? extends BaseTest>) Class.forName(className));
+    }
+
+    public static TestExecutionSummary launch(List<Class<? extends BaseTest>> testClasses) {
+        SummaryGeneratingListener listener = new SummaryGeneratingListener();
+
+        LauncherDiscoveryRequestBuilder requestBuilder = LauncherDiscoveryRequestBuilder.request();
+        testClasses
+                .forEach(testClass -> requestBuilder.selectors(selectClass(testClass)));
+
+        LauncherDiscoveryRequest request = requestBuilder.build();
+        Launcher launcher = LauncherFactory.create();
+        TestPlan testPlan = launcher.discover(request);
+        launcher.registerTestExecutionListeners(listener);
+        launcher.execute(request);
+
+        return listener.getSummary();
+    }
 }
