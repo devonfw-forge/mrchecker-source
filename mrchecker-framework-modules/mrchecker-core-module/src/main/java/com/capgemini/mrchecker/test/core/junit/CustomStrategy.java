aa78a778a9f1115764b6529c4067be2f6a3ce2c1
Mariusz Kowalski <mariusz.kowalski@capgemini.com>
2023-04-04 10:15:25 +0200

    Version 2023.04.04: CustomStrategy on core, dependencies for WebAPI module

--- mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/junit/CustomStrategy.java
+++ mrchecker-framework-modules/mrchecker-core-module/src/main/java/com/capgemini/mrchecker/test/core/junit/CustomStrategy.java
@@ -1,14 +1,14 @@
-package com.capgemini.mrchecker.playwright.junit;
+package com.capgemini.mrchecker.test.core.junit;
 
 import org.junit.platform.engine.ConfigurationParameters;
 import org.junit.platform.engine.support.hierarchical.ParallelExecutionConfiguration;
 import org.junit.platform.engine.support.hierarchical.ParallelExecutionConfigurationStrategy;
 
-//Used in Playwright test project pom surefire config to properly run tests parallel on JUnit 5 + Playwright
+//Used in test project pom surefire config to properly run tests parallel on JUnit 5
 //junit.jupiter.execution.parallel.config.strategy = custom
-//junit.jupiter.execution.parallel.config.custom.class=com.capgemini.mrchecker.playwright.junit.CustomStrategy
+//junit.jupiter.execution.parallel.config.custom.class=com.capgemini.mrchecker.test.core.junit.CustomStrategy
 public class CustomStrategy implements ParallelExecutionConfiguration, ParallelExecutionConfigurationStrategy {
-    private int threadCount = Integer.parseInt(System.getProperty("thread.count", "1"));
+    private final int threadCount = Integer.parseInt(System.getProperty("thread.count", "1"));
 
     @Override
     public int getParallelism() {
